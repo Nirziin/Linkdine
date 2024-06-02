db4a71a2851792742917fdc123e9274cd256669e
Nicolas Cadinot <131916325+N1co911@users.noreply.github.com>
2024-06-02 23:03:02 +0200

    commit bonhomme de neige

--- register.php
+++ register.php
@@ -10,6 +10,12 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+// Chemin vers l'image par défaut sur votre serveur
+$imagePath = 'Images/imagepardefaut.jpeg';
+
+// Lecture de l'image par défaut en tant que données binaires (blob)
+$imageData = file_get_contents($imagePath);
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = password_hash($_POST["password"], PASSWORD_BCRYPT);
@@ -18,13 +24,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $mail = $_POST["mail"];
     $naissance = $_POST["naissance"];
 
-    $sql = "INSERT INTO users (username, password, nom, prenom, naissance, mail) VALUES ('$username', '$password', '$nom', '$prenom', '$naissance', '$mail')";
+    $sql = "INSERT INTO users (username, password, nom, prenom, naissance, mail, image) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sssssss", $username, $password, $nom, $prenom, $naissance, $mail, $imageData);
 
-    if ($conn->query($sql) === TRUE) {
+    if ($stmt->execute()) {
         echo "Registration successful. <a href='index.html'>Login here</a>";
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
+    $stmt->close();
 }
 
 $conn->close();
