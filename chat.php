3ed2b14fce7e0dfd6e89ad5fd1bd81cac7ed2240
robinlamy <131693594+robinlamy@users.noreply.github.com>
2024-06-02 22:23:51 +0200

    finition de la messagerie

--- chat.php
+++ chat.php
@@ -40,24 +40,28 @@ $background_color = getUserBackgroundColor($user_id);
  }
 ?>
 
-<!DOCTYPE html>
-<html>
- <head>
- <meta charset="utf-8" />
- <title>Exemple Chat Texto</title>
- <link rel="stylesheet" href="chat.css" />
-  <link rel="stylesheet" href="global.css" />
-  <style>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>ECE-in</title>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="chat.css">
+    <link rel="stylesheet" type="text/css" href="global.css">
+    <style>
         body {
             background-image: <?php echo htmlspecialchars($background_color); ?> !important;
         }
     </style>
- </head>
- <body>
+</head>
+<body>
+
  <nav class = "wrapper">
 	<?php
 		include 'head.php';
 	?>
+  <br>
  <?php
  if (!isset($_SESSION['name'])){
  loginForm();
@@ -115,8 +119,26 @@ $background_color = getUserBackgroundColor($user_id);
  });
  });
  </script>
- </nav>
- </body>
+ 
+ <footer>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-6" style="border: solid black; padding: 2px;">
+                        <p style="margin-top: 10%;">
+                            Bienvenue sur Link dine, le plus grand réseau professionnel mondial comptant plus de 2 utilisateurs dans plus de 0 pays et territoires du monde.
+                        </p>
+                    </div>
+                    <div class="col-sm-6" style="border: solid black; padding: 2px;">
+                        <p style="text-align: center;">Nous contacter</p>
+                        <a href="mailto:romain.barriere@edu.ece.fr">Mail</a>
+                        <br>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.3661096301935!2d2.2859856116549255!3d48.851228701091536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6701b4f58251b%3A0x167f5a60fb94aa76!2sECE%20-%20Ecole%20d&#39;ing%C3%A9nieurs%20-%20Engineering%20school.!5e0!3m2!1sfr!2sfr!4v1685461093343!5m2!1sfr!2sfr" width="100" height="100" style="border: 0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </nav>
+</body>
 </html>
 <?php
 }
