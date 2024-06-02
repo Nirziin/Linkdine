54fe6a0b66a55052e4a217f9cdc9e4ac86f30f64
Nicolas Cadinot <131916325+N1co911@users.noreply.github.com>
2024-06-02 17:31:19 +0200

    ajout de la page admin

--- vous.php
+++ vous.php
@@ -62,7 +62,7 @@ $conn->close();
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
   <link rel="stylesheet" type="text/css" href="global.css">
-  <link rel="stylesheet" type="text/css" href="css/vous.css">
+  <link rel="stylesheet" type="text/css" href="vous.css">
 </head>
 <body>
   <nav class="wrapper">
