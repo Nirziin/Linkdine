d270f73fc44f2b59a8c3c02c481a395a9d2fdf30
Nicolas Cadinot <131916325+N1co911@users.noreply.github.com>
2024-06-02 22:28:27 +0200

    Update view_profile.php

--- view_profile.php
+++ view_profile.php
@@ -40,6 +40,11 @@ if ($result_user->num_rows > 0) {
 }
 
 $conn->close();
+
+function getImageSrc($image) {
+    return 'data:image/jpeg;base64,' . base64_encode($image);
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -63,6 +68,7 @@ $conn->close();
     <nav class="wrapper">
         <?php include 'head.php'; ?>
     <h2><?php echo $friend["username"]; ?>'s Profile</h2>
+    <img src="<?php echo getImageSrc($friend['image']); ?>" alt="Profile Picture">
     <p>Nom: <?php echo $friend["nom"]; echo " "; echo $friend["prenom"]; ?></p>
     <p>Bio: <?php echo $friend["bio"]; ?></p>
     <?php if ($result_posts->num_rows > 0): ?>
@@ -81,23 +87,24 @@ $conn->close();
     <?php else: ?>
         <p>Aucune publication récente.</p>
     <?php endif; ?>
-    <a href="profile.php">Retour au Profil</a>
-<footer>
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-sm-6" style="border: solid black; padding: 2px;">
-                        <p style="margin-top: 10%;">
-                            Bienvenue sur Link dine, le plus grand réseau professionnel mondial comptant plus de 2 utilisateurs dans plus de 0 pays et territoires du monde.
-                        </p>
-                    </div>
-                    <div class="col-sm-6" style="border: solid black; padding: 2px;">
-                        <p style="text-align: center;">Nous contacter</p>
-                        <a href="mailto:romain.barriere@edu.ece.fr">Mail</a>
-                        <br>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.3661096301935!2d2.2859856116549255!3d48.851228701091536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6701b4f58251b%3A0x167f5a60fb94aa76!2sECE%20-%20Ecole%20d&#39;ing%C3%A9nieurs%20-%20Engineering%20school.!5e0!3m2!1sfr!2sfr!4v1685461093343!5m2!1sfr!2sfr" width="100" height="100" style="border: 0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                    </div>
+    <a href="reseau.php">Retour au Profil</a>
+    <footer>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-6" style="border: solid black; padding: 2px;">
+                    <p style="margin-top: 10%;">
+                        Bienvenue sur Link dine, le plus grand réseau professionnel mondial comptant plus de 2 utilisateurs dans plus de 0 pays et territoires du monde.
+                    </p>
+                </div>
+                <div class="col-sm-6" style="border: solid black; padding: 2px;">
+                    <p style="text-align: center;">Nous contacter</p>
+                    <a href="mailto:romain.barriere@edu.ece.fr">Mail</a>
+                    <br>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.3661096301935!2d2.2859856116549255!3d48.851228701091536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6701b4f58251b%3A0x167f5a60fb94aa76!2sECE%20-%20Ecole%20d&#39;ing%C3%A9nieurs%20-%20Engineering%20school.!5e0!3m2!1sfr!2sfr!4v1685461093343!5m2!1sfr!2sfr" width="100" height="100" style="border: 0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
-        </footer>
+        </div>
+    </footer>
     </nav>
 </body>
+</html>
