d911c3db0789104fe3b0ddb64c5fda3620d0bc8b
robinlamy <131693594+robinlamy@users.noreply.github.com>
2024-06-02 18:18:15 +0200

    changement couleur + ajout css reseau

--- reseau.php
+++ reseau.php
@@ -42,9 +42,19 @@ $result_requests = $stmt_requests->get_result();
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Network</title>
+    <meta charset="UTF-8">
+    <title>ECE-in</title>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="emplois.css">
+    <link rel="stylesheet" type="text/css" href="global.css">
 </head>
 <body>
+    <nav class = "wrapper">
+        <?php
+        include 'head.php';
+    ?>
     <h2>Welcome, <?php echo $_SESSION["username"]; ?></h2>
     <h3>Your Friends</h3>
     <ul>
@@ -73,9 +83,27 @@ $result_requests = $stmt_requests->get_result();
         ?>
     </ul>
     <a href="add_friend.php">Add Friend</a>
+  <footer>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-6" style = "border : solid black; padding:2px">
+                        <p style = "margin-top:10%;">
+                            Bienvenue sur Link dine, le plus grand réseau professionnel mondial comptant plus de 2 utilisateurs dans plus de 0 pays et territoires du monde.
+                        </p>
+                    </div>
+                    <div class="col-sm-6" style = "border : solid black; padding:2px">
+                        <p style="text-align : center;">Nous contacter</p>
+
+                        <a href="mailto:romain.barriere@edu.ece.fr"> Mail </a>
+                        <br>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.3661096301935!2d2.2859856116549255!3d48.851228701091536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6701b4f58251b%3A0x167f5a60fb94aa76!2sECE%20-%20Ecole%20d&#39;ing%C3%A9nieurs%20-%20Engineering%20school.!5e0!3m2!1sfr!2sfr!4v1685461093343!5m2!1sfr!2sfr" width="100" height="100" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </nav>
 </body>
 </html>
-
 <?php
 $conn->close();
 ?>
