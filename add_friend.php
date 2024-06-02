9b9e46661f62d5dabe1fdff15e64986392fa28c2
Nicolas Cadinot <131916325+N1co911@users.noreply.github.com>
2024-06-02 21:52:07 +0200

    finitions sur reseau

--- add_friend.php
+++ add_friend.php
@@ -2,13 +2,14 @@
 <html>
 <head>
     <title>Add Friend</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
     <h2>Add Friend</h2>
     <form action="send_request.php" method="post">
         <label for="username">Friend's Username:</label>
         <input type="text" id="username" name="username" required><br>
-        <input type="submit" value="Send Request">
+        <input type="submit" class="btn btn-primary mt-4" value="Add Friend">
     </form>
 </body>
 </html>
